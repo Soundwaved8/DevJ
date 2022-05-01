0e6b0a09f4689ae79b510e308c116bdea0bcf7d1
Soundwave <51988999+Soundwaved8@users.noreply.github.com>
2022-05-01 23:44:29 +0200

    Delete APIFlightApp/src/main/java/com/example/APIFlightApp/repositories directory

--- APIFlightApp/src/main/java/com/example/APIFlightApp/repositories/HistoricPlaneRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.APIFlightApp.repositories;
-
-import com.example.APIFlightApp.models.Historic;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface HistoricPlaneRepository extends JpaRepository<Historic, String> {
-}
--- APIFlightApp/src/main/java/com/example/APIFlightApp/repositories/PlaneRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.APIFlightApp.repositories;
-
-import com.example.APIFlightApp.models.Plane;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PlaneRepository extends JpaRepository<Plane, String> {
-}
